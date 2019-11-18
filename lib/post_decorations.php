8b8e3eac13b40a8c33c83839a3477c81613e2e96
pawkw <31358773+pawkw@users.noreply.github.com>
2019-11-18 17:05:30 -0500

    i18n of  post  decorations

--- lib/post_decorations.php
+++ lib/post_decorations.php
@@ -1,13 +1,20 @@
 <?php
 
 function pawprint_post_meta() {
-    echo 'Posted on: <a href="'.esc_url(get_permalink()).'">';
-    echo '<time datetime="'.esc_attr(get_the_date('c')).'">'.esc_html(get_the_date()).'</time></a>';
-    echo ' by <a href="'.esc_url(get_author_posts_url( get_the_author_meta( 'ID'))).'">';
-    echo esc_html(get_the_author()).'</a>';
+    /* translators: $s is the date of the post */
+    printf(esc_html__('Posted on %s', 'pawprint'), '<a href="'.esc_url(get_permalink()).'"><time datetime="'.esc_attr(get_the_date('c')).'">'.esc_html(get_the_date()).'</time></a>');
+
+    /* translators: %s is the name of the author */
+    printf(esc_html__( ' by %s', 'pawprint' ), ' <a href="'.esc_url(get_author_posts_url( get_the_author_meta( 'ID'))).'">'.esc_html(get_the_author()).'</a>');
 }
 
 function pawprint_continue_reading() {
-    echo '<a href="'.esc_url(get_the_permalink()).'" title="'.the_title_attribute( ['echo' => false] ).'">Continue reading <span class="u-screen-reader-text">';
-    echo get_the_title().'</span></a>';
+    echo '<a href="'.esc_url(get_the_permalink()).'" title="'.the_title_attribute( ['echo' => false] ).'">';
+    printf(
+        /* translators: $s is the title of the post */
+        wp_kses( __( 'Continue reading <span class="u-screen-reader-text">%s</span></a>', 'pawprint'),
+        [ 
+            'span' => [ 'class' => [] ]
+        ]
+        ), get_the_title());
 }
