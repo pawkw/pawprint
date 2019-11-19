62e8a66262b5ea68f41e98b2608b1580608dd129
pawkw <31358773+pawkw@users.noreply.github.com>
2019-11-19 14:55:08 -0500

    Added file copying.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,14 +3,48 @@ import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
 
 const PRODUCTION = yargs.argv.prod;
 
-export const styles = (done) => {
-    return gulp.src('src/assets/scss/bundle.scss')
+const paths = {
+    styles: {
+        src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+        dest: 'dist/assets/css'
+    },
+    images: {
+        src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+        dest: 'dist/assets/images'
+    },
+    other: {
+        src: ['src/assets/**/*', '!src/assets/{scss,images,js}', '!src/assets/{scss,images,js}/**/*'],
+        dest: 'dist/assets'
+    }
+}
+
+export const styles = () => {
+    return gulp.src(paths.styles.src)
+        .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulpif(PRODUCTION, cleanCSS({ compatibility: 'ie8' })))
-        .pipe(gulp.dest('dist/assets/css'));
+        .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+export const images = () => {
+    return gulp.src(paths.images.src)
+        .pipe(gulpif(PRODUCTION, imagemin()))
+        .pipe(gulp.dest(paths.images.dest));
+}
+
+export const copy = () => {
+    return gulp.src(paths.other.src)
+        .pipe(gulp.dest(paths.other.dest));
+}
+
+export const watch = () => {
+    gulp.watch('src/assets/scss/**/*.scss', styles);
 }
 
 export default styles;
--- lib/enqueue_assets.php
+++ lib/enqueue_assets.php
@@ -4,4 +4,9 @@ function pawprint_assets() {
     wp_enqueue_style( 'pawprint_stylesheet', get_template_directory_uri().'/dist/assets/css/bundle.css' , array(), '1.0.0', 'all' );
 }
 
+function pawprint_admin_assets() {
+    wp_enqueue_style( 'pawprint_admin_stylesheet', get_template_directory_uri().'/dist/assets/css/admin.css' , array(), '1.0.0', 'all' );
+}
+
 add_action( 'wp_enqueue_scripts', 'pawprint_assets' );
+add_action( 'admin_enqueue_scripts', 'pawprint_admin_assets' );
